a3966299a2c1cfe0ffa9eba0a7350d8848ad864f
TotallyGamerJet <totallygamerjet@gmail.com>
2021-08-17 20:40:19 -0400

    Update glfw.go

--- libs/glfw.go
+++ libs/glfw.go
@@ -13,12 +13,17 @@ func init() {
 		hintT := types.NamedTGo("Hint", "glfw.Hint", env.Go().Int())
 		actionT := types.NamedTGo("Action", "glfw.Action", env.Go().Int())
 		keyT := types.NamedTGo("Key", "glfw.Key", env.Go().Int())
+		modKeyT := types.NamedTGo("ModifierKey", "glfw.ModifierKey", env.Go().Int())
+		keyCbT := types.NamedTGo("GLFWkeyfun", "glfw.KeyCallback", env.FuncTT(nil, keyT, env.Go().Int(), actionT, modKeyT))
 		windowT := types.NamedTGo("GLFWwindow", "glfw.Window", env.MethStructT(map[string]*types.FuncType{
 			"MakeContextCurrent": env.FuncTT(nil, nil),
 			"ShouldClose":        env.FuncTT(env.Go().Bool(), nil),
 			"SwapBuffers":        env.FuncTT(nil, nil),
 			"GetKey":             env.FuncTT(actionT, keyT),
 			"SetShouldClose":     env.FuncTT(nil, env.Go().Bool()),
+			"SetKeyCallback":     env.FuncTT(keyCbT, keyCbT),
+			"GetFramebufferSize": env.FuncTT(nil, env.Go().Int(), env.Go().Int()), //FIXME: incorrect signature
+			"Destroy":            env.FuncTT(nil, nil),
 		}))
 		monitorT := types.NamedTGo("GLFWmonitor", "glfw.Monitor", types.StructT(nil))
 		l := &Library{
@@ -37,7 +42,7 @@ func init() {
 				"GLFW_OPENGL_PROFILE":        types.NewIdentGo("GLFW_OPENGL_PROFILE", "glfw.OpenGLProfile", hintT),
 				"GLFW_OPENGL_CORE_PROFILE":   types.NewIdentGo("GLFW_OPENGL_CORE_PROFILE", "glfw.OpenGLCoreProfile", env.Go().Int()),
 				"GLFW_OPENGL_FORWARD_COMPAT": types.NewIdentGo("GLFW_OPENGL_FORWARD_COMPAT", "glfw.OpenGLForwardCompatible", hintT),
-				"GL_TRUE":                    types.NewIdentGo("GL_TRUE", "glfw.True", env.Go().Int()),
+				"GLFW_TRUE":                  types.NewIdentGo("GLFW_TRUE", "glfw.True", env.Go().Int()),
 				"GLFW_PRESS":                 types.NewIdentGo("GLFW_PRESS", "glfw.Press", actionT),
 				"GLFW_KEY_ESCAPE":            types.NewIdentGo("GLFW_KEY_ESCAPE", "glfw.KeyEscape", env.Go().Int()),
 			},
@@ -47,13 +52,15 @@ const int GLFW_CONTEXT_VERSION_MINOR = 0x00022003;
 const int GLFW_OPENGL_PROFILE = 0x00022008;
 const int GLFW_OPENGL_CORE_PROFILE = 0x00032001;
 const int GLFW_OPENGL_FORWARD_COMPAT = 0x00022006;
-const int GL_TRUE = 1;
+const int GLFW_TRUE = 1;
 const int GLFW_PRESS = 1;
 
 // functions keys
 const int GLFW_KEY_ESCAPE = 256;
 
 typedef struct GLFWwindow GLFWwindow;
+typedef void (* GLFWerrorfun)(int,const char*);
+typedef void (* GLFWkeyfun)(GLFWwindow*,int,int,int,int);
 typedef void (* GLFWframebuffersizefun)(GLFWwindow*,int,int);
 
 struct GLFWwindow {
@@ -62,16 +69,23 @@ struct GLFWwindow {
 	void (*SwapBuffers)(void);
 	int (*GetKey)(int);
 	void (*SetShouldClose)(_Bool);
+	void (*GetFramebufferSize)(int* width, int* height);
+	void (*Destroy)();
+	// callbacks
+	GLFWframebuffersizefun (*SetKeyCallback)(GLFWframebuffersizefun)
 };
 #define glfwMakeContextCurrent(win) ((GLFWwindow*)win)->MakeContextCurrent()
 #define glfwWindowShouldClose(win) ((GLFWwindow*)win)->ShouldClose()
 #define glfwSwapBuffers(win) ((GLFWwindow*)win)->SwapBuffers()
 #define glfwGetKey(win, k) ((GLFWwindow*)win)->GetKey(k)
 #define glfwSetWindowShouldClose(win, b) ((GLFWwindow*)win)->SetShouldClose(b)
-
+#define glfwSetKeyCallback(win, cb) ((GLFWwindow*)win)->SetKeyCallback(cb)
+#define glfwGetFramebufferSize(win, w, h) ((GLFWwindow*)win)->GetFramebufferSize(w, h)
+#define glfwDestroyWindow(win) ((GLFWwindow*)win)->Destroy()
 typedef struct GLFWmonitor GLFWmonitor;
 
 void glfwWindowHint(int, int);
+GLFWerrorfun glfwSetErrorCallback(GLFWerrorfun callback); // no go equivalent
 `,
 		}
 		l.Declare(
@@ -81,6 +95,8 @@ void glfwWindowHint(int, int);
 			types.NewIdentGo("glfwCreateWindow", "glfw.CreateWindow", env.FuncTT(env.PtrT(windowT), env.Go().Int(), env.Go().Int(), env.Go().String(), env.PtrT(monitorT), env.PtrT(windowT))),
 			types.NewIdentGo("glfwGetProcAddress", "glfw.GetProcAddress", env.FuncTT(env.PtrT(nil), env.Go().String())),
 			types.NewIdentGo("glfwPollEvents", "glfw.PollEvents", env.FuncTT(nil, nil)),
+			types.NewIdentGo("glfwSwapInterval", "glfw.SwapInterval", env.FuncTT(nil, env.Go().Int())),
+			types.NewIdentGo("glfwGetTime", "glfw.GetTime", env.FuncTT(env.C().Float(), nil)),
 		)
 		return l
 	})
