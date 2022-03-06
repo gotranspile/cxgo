66ff73495ab74c8810afbdaa0b3ffc1f74cd81cf
Denys Smirnov <dennwc@pm.me>
2022-03-06 16:53:26 +0200

    libs: bare minimum support for semaphore.h

---
+++ libs/semaphore.go
@@ -0,0 +1,30 @@
+package libs
+
+const (
+	semaphoreH = "semaphore.h"
+)
+
+func init() {
+	RegisterLibrary(semaphoreH, func(c *Env) *Library {
+		return &Library{
+			// TODO
+			Header: `
+#include <` + BuiltinH + `>
+#include <` + timeH + `>
+
+typedef struct {} sem_t;
+
+int    sem_close(sem_t *);
+int    sem_destroy(sem_t *);
+int    sem_getvalue(sem_t *, int *);
+int    sem_init(sem_t *, int, unsigned);
+sem_t *sem_open(const char *, int, ...);
+int    sem_post(sem_t *);
+int    sem_timedwait(sem_t *, const struct timespec *);
+int    sem_trywait(sem_t *);
+int    sem_unlink(const char *);
+int    sem_wait(sem_t *);
+`,
+		}
+	})
+}
