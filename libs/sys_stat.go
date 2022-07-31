0001473901311ecf8f463295a11519c39f8543cd
Denys Smirnov <dennwc@pm.me>
2022-07-31 12:59:36 +0300

    libs: more support for sys/stat.h

--- libs/sys_stat.go
+++ libs/sys_stat.go
@@ -24,9 +24,9 @@ func init() {
 			{Name: types.NewIdentGo("st_gid", "GID", intT)},
 			{Name: types.NewIdentGo("st_rdev", "RDev", intT)},
 			{Name: types.NewIdentGo("st_size", "Size", types.UintT(8))},
-			{Name: types.NewIdentGo("st_atim", "ATime", timevalT)},
-			{Name: types.NewIdentGo("st_mtim", "MTime", timevalT)},
-			{Name: types.NewIdentGo("st_ctim", "CTime", timevalT)},
+			{Name: types.NewIdentGo("st_atime", "ATime", timevalT)},
+			{Name: types.NewIdentGo("st_mtime", "MTime", timevalT)},
+			{Name: types.NewIdentGo("st_ctime", "CTime", timevalT)},
 			{Name: types.NewIdentGo("st_blksize", "BlockSize", intT)},
 			{Name: types.NewIdentGo("st_blocks", "Blocks", intT)},
 		}))
@@ -60,9 +60,9 @@ struct stat {
     _cxgo_sint32       st_gid;     /* group ID of owner */
     _cxgo_sint32       st_rdev;    /* device ID (if special file) */
     off_t       st_size;    /* total size, in bytes */
-    struct timeval      st_atim;   /* time of last access */
-    struct timeval      st_mtim;   /* time of last modification */
-    struct timeval      st_ctim;   /* time of last status change */
+    struct timeval      st_atime;   /* time of last access */
+    struct timeval      st_mtime;   /* time of last modification */
+    struct timeval      st_ctime;   /* time of last status change */
     _cxgo_sint32   st_blksize; /* blocksize for filesystem I/O */
     _cxgo_sint32    st_blocks;  /* number of blocks allocated */
 };
@@ -77,6 +77,33 @@ _cxgo_sint32    stat(const char *restrict, struct stat *restrict);
 mode_t umask(mode_t);
 
 _cxgo_sint32 S_ISDIR(mode_t m);
+
+#define S_IRUSR 1
+#define S_IWUSR 1
+#define S_IXUSR 1
+
+#define S_IRGRP 1
+#define S_IWGRP 1
+#define S_IXGRP 1
+
+#define S_IROTH 1
+#define S_IWOTH 1
+#define S_IXOTH 1
+
+#define S_ISUID 1
+#define S_ISGID 1
+#define S_ISVTX 1
+
+#define S_IFMT 1
+#define S_IFLNK 1
+#define S_IFREG 1
+#define S_IFCHR 1
+#define S_IFBLK 1
+#define S_IFIFO 1
+#define S_IFSOCK 1
+#define S_IFDIR 1
+
+int S_ISLNK(int);
 `,
 		}
 	})
