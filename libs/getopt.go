db6c9de3d3576291afde953fb58f107552109387
Denys Smirnov <dennwc@pm.me>
2022-07-31 12:59:32 +0300

    libs: more options for getopt.h

--- libs/getopt.go
+++ libs/getopt.go
@@ -22,7 +22,7 @@ int getopt_long(int argc, char * const argv[], const char *optstring,
 int getopt_long_only(int argc, char * const argv[], const char *optstring,
                   const struct option *longopts, int *longindex);
 char *optarg;
-int optind, opterr, optopt;
+int optind, opterr, optopt, required_argument, no_argument;
 `,
 		}
 	})
